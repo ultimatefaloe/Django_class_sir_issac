437e297da62873c8bcdeb0ab6d2613f43dc677ec
ultimatefaloe <ultimatefaloe@outlool.com>
2026-06-24 13:36:25 +0100

    class 5

--- django_3_w/project/urls.py
+++ django_3_w/project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('posts/', include('posts.urls', namespace='posts')),
 ]
